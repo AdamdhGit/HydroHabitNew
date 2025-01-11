76ae81c4b95c5d4d4c64495807522d8b5bf108ab
Adam Heidmann <adamdhm@yahoo.com>
2025-01-11 09:55:12 -0800

    Initial Commit

---
+++ HydroHabit2/HydroHabit2App.swift
@@ -0,0 +1,17 @@
+//
+//  HydroHabit2App.swift
+//  HydroHabit2
+//
+//  Created by Adam Heidmann on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct HydroHabit2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HydroHabit2Tests/HydroHabit2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  HydroHabit2Tests.swift
+//  HydroHabit2Tests
+//
+//  Created by Adam Heidmann on 1/11/25.
+//
+
+import Testing
+@testable import HydroHabit2
+
+struct HydroHabit2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
